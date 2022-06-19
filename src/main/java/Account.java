9dbe68e299eff02b1db2cb76d9189307dd075919
konstantin_deripalko <k.deripalko@lipt-soft.ru>
2022-06-19 22:04:00 +0300

    Sprint_4 add tests

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -6,27 +6,34 @@ public class Account {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+
     public boolean checkNameToEmboss() {
         /*
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
-
-        // считаем количество нулей
-        long countSpace = name.chars().filter(ch -> ch == ' ').count();
-        // узнаем индекс пробела в имени
-        int indexSpace = name.indexOf(' ');
-
-        // проверка на длину от 3 до 19 символов включительно
-        if (name.length() >= 3 && name.length() <= 19) {
-            // проверка на то что в имени есть 1 пробел
-            if (countSpace == 1) {
-                // и этот пробел не в начале и не в конце имени
-                if (indexSpace != 0 && indexSpace != name.length() - 1) {
-                    return true;
+        try {
+            // считаем количество пробелов
+            long countSpace = name.chars().filter(ch -> ch == ' ').count();
+            // узнаем индекс пробела в имени
+            int indexSpace = name.indexOf(' ');
+            // проверка на длину от 3 до 19 символов включительно
+            if (name.length() >= 3 && name.length() <= 19) {
+                // проверка на то что в имени есть 1 пробел
+                if (countSpace == 1) {
+                    // и этот пробел не в начале и не в конце имени
+                    if (indexSpace != 0 && indexSpace != name.length() - 1) {
+                        return true;
+                    }
                 }
             }
+            return false;
+        } catch (NullPointerException e) {
+            return false;
         }
-        return false;
     }
 }
